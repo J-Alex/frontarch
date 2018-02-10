01c70c3387b98c16cb3bbab4327e73766fd8ecf0
J-Alex <jlex.wd@outlook.com>
2018-02-10 17:09:21 -0600

    sabado 09 Feb - Arch

--- src/js/components/contactoComponent/contacto.ctrl.js
+++ src/js/components/contactoComponent/contacto.ctrl.js
@@ -1,9 +1,25 @@
-
 class contactoCtrl {
-    constructor(){
+    constructor($document, $http){
         'ngInject'
+
+        this.$document = $document;
+        this.$http = $http;
+
+        this.mailObj = {};
     }
-    $onInit(){
-    }
+    $onInit(){}
+    
+    sendMail() {
+        let formulario = this.$document.find('.contact-form')[0];
+        console.log(this.mailObj);
+        
+        this.$http.post("../../api/mail-api.php", this.mailObj)
+        .then(function(data,status,headers,config){
+            console.log(data);
+            formulario.reset();
+        },function(err,status,headers,config){
+            console.log(err);
+        });
+    };
 }
 export default contactoCtrl;
