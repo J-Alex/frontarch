8c2aff4a9237d430ae96d872bd378170e754a67d
Herald-Flores <heraldflores95@gmail.com>
2018-04-07 23:21:56 -0600

    Portafolio

--- src/js/components/servicioDevComponent/servicioDev.ctrl.js
+++ src/js/components/servicioDevComponent/servicioDev.ctrl.js
@@ -12,5 +12,24 @@ class servicioDevCtrl {
         this.$document.find('html,body').stop().animate({scrollTop: targetOffset}, 600);
         console.log(targetOffset);        
     }
+
+        
+    open($namemodal,$btnOpen,$close){
+        // modal web
+        var modal = document.getElementById($namemodal);
+        var btn = document.getElementById($btnOpen);
+        var span = document.getElementsByClassName($close)[0];
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    }
 }
 export default servicioDevCtrl;
